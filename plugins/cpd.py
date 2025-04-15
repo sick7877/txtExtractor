7f1b6aa3e54416f8eb6d47c8988f2d365e326a13
CHITIJRAJPUTX <whitedevil8126@gmail.com>
2025-04-15 13:33:40 +0530

    Update cpd.py

--- plugins/cpd.py
+++ plugins/cpd.py
@@ -1,119 +1,78 @@
-#  MIT License
-#
-#  Copyright (c) 2019-present Dan <https://github.com/delivrance>
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE
-#  Code edited By Cryptostark
-import urllib
-import urllib.parse
-import requests
-import json
-import subprocess
-from pyrogram.types.messages_and_media import message
-import helper
-from pyromod import listen
-from pyrogram.types import Message
-import tgcrypto
-import pyrogram
-from pyrogram import Client, filters
-from pyrogram.types.messages_and_media import message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import FloodWait
-import time
-from pyrogram.types import User, Message
-from p_bar import progress_bar
-from subprocess import getstatusoutput
-import logging
+# MIT License
+# Copyright (c) 2019-present Dan <https://github.com/delivrance>
+
 import os
-import sys
 import re
-from pyrogram import Client as bot
+import sys
+import time
+import json
+import logging
+import requests
+import subprocess
+from base64 import b64decode
+from subprocess import getstatusoutput
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import FloodWait
+from pyromod import listen
+import tgcrypto
 import cloudscraper
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
-from base64 import b64encode, b64decode
 
-@bot.on_message(filters.command(["cpd"])
+import helper
+from p_bar import progress_bar
+
+@Client.on_message(filters.command(["cpd"]) & ~filters.edited)
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("Send txt file**")
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await input.delete(True)
 
-    path = f"./downloads/{m.chat.id}"
-
-    try:    
+    try:
         with open(x, "r") as f:
-            content = f.read()
-        content = content.split("\n")
-        links = []
-        for i in content:
-            links.append(i.split(":", 1))
+            content = f.read().split("\n")
+        links = [i.split(":", 1) for i in content]
         os.remove(x)
-        # print(len(links))
     except:
         await m.reply_text("Invalid file input.")
         os.remove(x)
         return
 
-    editable = await m.reply_text(f"Total links found are **{len(links)}**\n\nSend From where you want to download initial is **0**")
+    editable = await m.reply_text(f"Total links found: **{len(links)}**\n\nSend start index (default is **0**):")
     input1: Message = await bot.listen(editable.chat.id)
-    raw_text = input1.text
-
-
     try:
-        arg = int(raw_text)
+        start_index = int(input1.text)
     except:
-        arg = 0
-    
-    
+        start_index = 0
+
     editable = await m.reply_text("**Enter Title**")
-    input0: Message = await bot.listen(editable.chat.id)
-    raw_text0 = input0.text
-    
+    input_title: Message = await bot.listen(editable.chat.id)
+    title = input_title.text
+
     await m.reply_text("**Enter resolution**")
-    input2: Message = await bot.listen(editable.chat.id)
-    raw_text2 = input2.text
+    input_res: Message = await bot.listen(editable.chat.id)
+    resolution = input_res.text
 
-    editable4= await m.reply_text("Now send the **Thumb url**\nEg : ```https://telegra.ph/file/d9e24878bd4aba05049a1.jpg```\n\nor Send **no**")
-    input6 = message = await bot.listen(editable.chat.id)
-    raw_text6 = input6.text
-
-    thumb = input6.text
+    editable = await m.reply_text("Now send the **Thumb URL** or type **no**")
+    input_thumb: Message = await bot.listen(editable.chat.id)
+    thumb = input_thumb.text
     if thumb.startswith("http://") or thumb.startswith("https://"):
         getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
         thumb = "thumb.jpg"
     else:
-        thumb == "no"
-        
-    if raw_text =='0':
-        count =1
-    else:       
-        count =int(raw_text)    
+        thumb = "no"
 
-    
+    count = start_index + 1
+
     try:
-        for i in range(arg, len(links)):
-            
+        for i in range(start_index, len(links)):
+            name_part = links[i][0].translate(str.maketrans('', '', '\t:/+|@#*.')).strip()
             url = links[i][1]
-            name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@","").replace("*","").replace(".","").strip()
-            
+            name = f"{str(count).zfill(3)}) {name_part}"
+
             if "classplus" in url:
                 headers = {
                     'Host': 'api.classplusapp.com',
@@ -125,78 +84,69 @@ async def account_login(bot: Client, m: Message):
                     'device-details': '2848b866799971ca_2848b8667a33216c_SDK-30',
                     'accept-encoding': 'gzip',
                 }
-
-                params = (
-                    ('url', f'{url}'),
-                )
-
-                response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
-                # print(response.json())
-                a = response.json()['url']
-                # print(a)
-
-
-                headers1 = {
-                    'User-Agent': 'ExoPlayerDemo/1.4.37.1 (Linux;Android 11) ExoPlayerLib/2.14.1',
-                    'Accept-Encoding': 'gzip',
-                    'Host': 'cdn.jwplayer.com',
-                    'Connection': 'Keep-Alive',
-                }
-
-
-                response1 = requests.get(f'{a}', headers=headers1)
-
-                url1 = (response1.text).split("\n")[2]
+                params = (('url', url),)
+                res = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
+                url1 = res.json().get("url", "")
+                res1 = requests.get(url1, headers={'User-Agent': 'ExoPlayerDemo/1.4.37.1'})
+                url1 = res1.text.split("\n")[2]
             else:
                 url1 = url
 
-                
-            name = f'{str(count).zfill(3)}) {name1}'    
-            Show = f"**Downloading:-**\n\n**Name :-** `{name}`\n\n**Url :-** `{url1}`"
-            prog = await m.reply_text(Show)
-            cc = f'**Title »** {name1}.mkv\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}'
+            prog = await m.reply_text(f"**Downloading:**\n\n**Name:** `{name}`\n**URL:** `{url1}`")
+            caption = f'**Title »** {name_part}.mkv\n**Caption »** {title}\n**Index »** {str(count).zfill(3)}'
+
             if "pdf" in url:
                 cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
             else:
                 cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --no-check-certificate --remux-video mkv "{url1}"'
+
             try:
                 download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
                 os.system(download_cmd)
-                
 
                 if os.path.isfile(f"{name}.mkv"):
                     filename = f"{name}.mkv"
                 elif os.path.isfile(f"{name}.mp4"):
-                    filename = f"{name}.mp4"  
+                    filename = f"{name}.mp4"
                 elif os.path.isfile(f"{name}.pdf"):
-                    filename = f"{name}.pdf"  
-                subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
-                await prog.delete (True)
-                reply = await m.reply_text(f"Uploading - ```{name}```")
-                try:
-                    if thumb == "no":
-                        thumbnail = f"{filename}.jpg"
-                    else:
-                        thumbnail = thumb
-                except Exception as e:
-                    await m.reply_text(str(e))
-
-                dur = int(helper.duration(filename))
-
-                start_time = time.time()
-                if "pdf" in url1:
-                    await m.reply_document(filename,caption=cc)
+                    filename = f"{name}.pdf"
                 else:
-                    await m.reply_video(filename,supports_streaming=True,height=720,width=1280,caption=cc,duration=dur,thumb=thumbnail, progress=progress_bar,progress_args=(reply,start_time) )
-                count+=1
-                os.remove(filename)
+                    raise Exception("Download failed.")
 
+                subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
+                await prog.delete(True)
+                reply = await m.reply_text(f"Uploading - ```{name}```")
+
+                thumbnail = thumb if thumb != "no" else f"{filename}.jpg"
+                duration = int(helper.duration(filename))
+                start_time = time.time()
+
+                if "pdf" in url1:
+                    await m.reply_document(filename, caption=caption)
+                else:
+                    await m.reply_video(
+                        filename,
+                        supports_streaming=True,
+                        height=720,
+                        width=1280,
+                        caption=caption,
+                        duration=duration,
+                        thumb=thumbnail,
+                        progress=progress_bar,
+                        progress_args=(reply, start_time),
+                    )
+
+                count += 1
+                os.remove(filename)
                 os.remove(f"{filename}.jpg")
-                await reply.delete (True)
+                await reply.delete(True)
                 time.sleep(1)
+
             except Exception as e:
-                await m.reply_text(f"**downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}` & `{url1}`")
-                continue 
+                await m.reply_text(f"**Download failed ❌**\n{str(e)}\n**Name:** {name}\n**Link:** `{url}` & `{url1}`")
+                continue
+
     except Exception as e:
-        await m.reply_text(e)
-    await m.reply_text("Done")          
+        await m.reply_text(str(e))
+
+    await m.reply_text("Done.")
