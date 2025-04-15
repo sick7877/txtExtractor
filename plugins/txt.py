191ecba7958380d6a6f2d7f03132d39903df2fa1
CHITIJRAJPUTX <whitedevil8126@gmail.com>
2025-04-16 00:06:05 +0530

    Update txt.py

--- plugins/txt.py
+++ plugins/txt.py
@@ -1,284 +1,158 @@
-#  MIT License
-#
-#  Copyright (c) 2019-present Dan <https://github.com/delivrance>
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE
-#  Code edited By Cryptostark
-import urllib
-import urllib.parse
-import requests
-import json
-import subprocess
-from pyrogram.types.messages_and_media import message
-import helper
-from pyromod import listen
-from pyrogram.types import Message
-import tgcrypto
-import pyrogram
-from pyrogram import Client, filters
-from pyrogram.types.messages_and_media import message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import FloodWait
-import time
-from pyrogram.types import User, Message
-from p_bar import progress_bar
-from subprocess import getstatusoutput
-import logging
+# MIT License
+# Copyright (c) 2019-present Dan
+# Edited by Cryptostark
+
 import os
 import sys
+import json
+import time
 import re
-from pyrogram import Client as bot
-import cloudscraper
+import logging
+import requests
+import urllib
+import subprocess
+from base64 import b64encode, b64decode
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
-from base64 import b64encode, b64decode
+from subprocess import getstatusoutput
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, User
+from pyromod import listen
+import tgcrypto
+from p_bar import progress_bar
+import helper
+import cloudscraper
 
-@stark.on_edited_message(filters.command(["start"]))
-async def account_login(bot: Client, m: Message):
+# Instantiate the bot properly
+bot = Client("my_bot")
+
+@bot.on_edited_message(filters.command(["start"]))
+async def account_login(client: Client, m: Message):
     global cancel
     cancel = False
-    editable = await m.reply_text("Copy & Paste any one og above Institute Code Example like this **Ankit With Rojgar** :- **rozgarapinew.teachx.in**")
-    editable = await m.reply_text("Copy & Paste any one og above Institute Code Example like this **The Last Exam**     :- **lastexamapi.teachx.in**")
-    editable = await m.reply_text("Copy & Paste any one og above Institute Code Example like this **The Mission Institute** :- **missionapi.appx.co.in**")
-    input01: Message = await bot.listen(editable.chat.id)
-    Ins = input01.text
-    editable = await m.reply_text("Send **ID & Password** in this manner otherwise bot will not respond.\n\nSend like this:-  **ID*Password**")
-    rwa_url = "https://"+Ins+"/post/login"
-    hdr = {"Client-Service": "Appx",
-           "Auth-Key": "appxapi",
-           "User-ID": "-2",
-           "Authorization": "",
-           "User_app_category": "",
-           "Language": "en",
-           "Content-Type": "application/x-www-form-urlencoded",
-           "Content-Length": "236",
-           "Accept-Encoding": "gzip, deflate",
-           "User-Agent": "okhttp/4.9.1"
-           }
-    info = {"email": "", "password": ""}
-    input1: Message = await bot.listen(editable.chat.id)
+
+    await m.reply_text("Copy & Paste any one of the Institute Codes:\n"
+                       "**Ankit With Rojgar** :- **rozgarapinew.teachx.in**\n"
+                       "**The Last Exam** :- **lastexamapi.teachx.in**\n"
+                       "**The Mission Institute** :- **missionapi.appx.co.in**")
+
+    input01: Message = await bot.listen(m.chat.id)
+    Ins = input01.text.strip()
+
+    await m.reply_text("Send **ID & Password** in format: `ID*Password`")
+    input1: Message = await bot.listen(m.chat.id)
     raw_text = input1.text
-    info["email"] = raw_text.split("*")[0]
-    info["password"] = raw_text.split("*")[1]
-    await input1.delete(True)
-    res = requests.post(rwa_url, data=info, headers=hdr).content
+    email, password = raw_text.split("*")
+
+    # Login API
+    login_url = f"https://{Ins}/post/login"
+    headers = {
+        "Client-Service": "Appx",
+        "Auth-Key": "appxapi",
+        "User-ID": "-2",
+        "Authorization": "",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Language": "en",
+        "User-Agent": "okhttp/4.9.1"
+    }
+    data = {"email": email, "password": password}
+    res = requests.post(login_url, data=data, headers=headers).content
     output = json.loads(res)
-    #print(output)
-    userid = output["data"]["userid"]
-    token = output["data"]["token"]
-    #print(userid)
-    #print(token)
-    if Ins == ("lastexamapi.teachx.in"):
-        hdr1 = {
-                "Client-Service": "Appx",
-                "Auth-Key": "appxapi",
-                "User-ID": userid,
-                "Authorization": token,
-                "User_app_category": "",
-                "Language": "en",
-                "Host": "lastexamapi.teachx.in",
-                "User-Agent": "okhttp/4.9.1"
-                }
-    elif Ins == ("missionapi.appx.co.in"):
-        hdr1 = {
-                "Client-Service": "Appx",
-                "Auth-Key": "appxapi",
-                "User-ID": userid,
-                "Authorization": token,
-                "User_app_category": "",
-                "Language": "en",
-                "Host": "missionapi.appx.co.in",
-                "User-Agent": "okhttp/4.9.1"
-                }
-    elif Ins == ("rozgarapinew.teachx.in"):
-        hdr1 = {
-                "Client-Service": "Appx",
-                "Auth-Key": "appxapi",
-                "User-ID": userid,
-                "Authorization": token,
-                "User_app_category": "",
-                "Language": "en",
-                "Host": "rozgarapinew.teachx.in",
-                "User-Agent": "okhttp/4.9.1"
-                }
-    else:
-        await editable.edit("**Header Not Valid**")
-    
-    await editable.edit("**login Successful**")
-    #cour_url = "https://rozgarapinew.teachx.in/get/mycourse?userid="
 
-    res1 = requests.get("https://"+Ins+"/get/mycourse?userid="+userid, headers=hdr1)
+    try:
+        userid = output["data"]["userid"]
+        token = output["data"]["token"]
+    except:
+        await m.reply_text("Login failed or invalid credentials.")
+        return
+
+    host_header = Ins
+    hdr1 = {
+        "Client-Service": "Appx",
+        "Auth-Key": "appxapi",
+        "User-ID": userid,
+        "Authorization": token,
+        "Language": "en",
+        "Host": host_header,
+        "User-Agent": "okhttp/4.9.1"
+    }
+
+    await m.reply_text("**Login Successful**")
+
+    # Fetch batch
+    batch_url = f"https://{Ins}/get/mycourse?userid={userid}"
+    res1 = requests.get(batch_url, headers=hdr1)
     b_data = res1.json()['data']
-    cool = ""
+    course_list = "**BATCH-ID - BATCH NAME - INSTRUCTOR**\n\n"
+
     for data in b_data:
-        t_name =data['course_name']
-        FFF = "**BATCH-ID - BATCH NAME - INSTRUCTOR**"
-        aa = f" ```{data['id']}```      - **{data['course_name']}**\n\n"
-        # aa=f"**Batch Name -** {data['batchName']}\n**Batch ID -** ```{data['id']}```\n**By -** {data['instructorName']}\n\n"
-        if len(f'{cool}{aa}') > 4096:
-            print(aa)
-            cool = ""
-        cool += aa
-    await editable.edit(f'{"**You have these batches :-**"}\n\n{FFF}\n\n{cool}')
-    editable1 = await m.reply_text("**Now send the Batch ID to Download**")
-    input2 = message = await bot.listen(editable.chat.id)
-    raw_text2 = input2.text
+        course_list += f"```{data['id']}``` - **{data['course_name']}**\n"
 
-    # sub_id_url="https://rozgarapinew.teachx.in/get/allsubjectfrmlivecourseclass?courseid="
-    res2 = requests.get("https://"+Ins+"/get/allsubjectfrmlivecourseclass?courseid="+raw_text2, headers=hdr1).json()
-    subjID = res2["data"]
-    await m.reply_text(subjID)
-    #SubiD = input("Enter the Subject Id Show in above Response")
+    await m.reply_text(course_list)
+    await m.reply_text("**Now send the Batch ID to Download**")
+    input2 = await bot.listen(m.chat.id)
+    course_id = input2.text.strip()
 
-    editable1 = await m.reply_text("**Enter the Subject Id Show in above Response")
-    input3 = message = await bot.listen(editable.chat.id)
-    raw_text3 = input3.text
+    subject_url = f"https://{Ins}/get/allsubjectfrmlivecourseclass?courseid={course_id}"
+    res2 = requests.get(subject_url, headers=hdr1).json()
+    await m.reply_text(res2["data"])
 
-    res3 = requests.get("https://"+Ins+"/get/alltopicfrmlivecourseclass?courseid="+raw_text2,"&subjectid="+raw_text3, headers=hdr1)
+    await m.reply_text("**Enter the Subject ID shown above**")
+    input3 = await bot.listen(m.chat.id)
+    subject_id = input3.text.strip()
+
+    topic_url = f"https://{Ins}/get/alltopicfrmlivecourseclass?courseid={course_id}&subjectid={subject_id}"
+    res3 = requests.get(topic_url, headers=hdr1)
     b_data2 = res3.json()['data']
-    # print(b_data2)
+
+    topic_list = "**TOPIC-ID - TOPIC - VIDEOS**\n\n"
     vj = ""
     for data in b_data2:
-        tids = (data["topicid"])
-        idid = f"{tids}&"
-        if len(f"{vj}{idid}") > 9999:
-            ##await m.reply_text(idid)
-            vj = ""
-        vj += idid
-    #print(vj)
-    vp = ""
-    for data in b_data2:
-        tn = (data["topic_name"])
-        tns = f"{tn}&"
-        if len(f"{vp}{tn}") > 9999:
-            ##await m.reply_text(tns)
-            vp = ""
-        vp += tns
-    #print(vp)
-    cool1 = ""
-    #BBB = ''
-    for data in b_data2:
-        t_name = (data["topic_name"])
-        tid = (data["topicid"])
-        zz = len(tid)
-        BBB = f"{'**TOPIC-ID    - TOPIC     - VIDEOS**'}\n"
-        hh = f"```{tid}```     - **{t_name} - ({zz})**\n"
-        #hh = f"**Topic -** {t_name}\n**Topic ID - ** ```{tid}```\nno. of videos are : {zz}\n\n"
-        if len(f'{cool1}{hh}') > 9999:
-            cool1 = ""
-        cool1 += hh
-    await m.reply_text(f'Batch details of **{t_name}** are:\n\n{BBB}\n\n{cool1}')
+        topic_list += f"```{data['topicid']}``` - **{data['topic_name']}**\n"
+        vj += f"{data['topicid']}&"
 
-    editable= await m.reply_text(f"Now send the **Topic IDs** to Download\n\nSend like this **1&2&3&4** so on\nor copy paste or edit **below ids** according to you :\n\n**Enter this to download full batch :-**\n```{vj}```")
-    input4 = message = await bot.listen(editable.chat.id)
-    raw_text4 = input4.text
+    await m.reply_text(topic_list)
+    await m.reply_text(f"Now send the **Topic IDs** to Download\n"
+                       f"Send like this **1&2&3** or use this:\n```{vj.strip('&')}```")
 
-    editable3 = await m.reply_text("**Now send the Resolution**")
-    input5 = message = await bot.listen(editable.chat.id)
-    raw_text5 = input5.text
+    input4 = await bot.listen(m.chat.id)
+    topic_ids = input4.text.strip().split("&")
+
+    await m.reply_text("**Now send the Resolution**")
+    input5 = await bot.listen(m.chat.id)
+    resolution = input5.text.strip()
+
     try:
-        xv = raw_text4.split('&')
-        for y in range(0,len(xv)):
-            t =xv[y]
-            if Ins == ("lastexamapi.teachx.in"):
-                hdr11 = {
-                    "Host": "lastexamapi.teachx.in",
-                    "Client-Service": "Appx",
-                    "Auth-Key": "appxapi",
-                    "User-Id": userid,
-                    "Authorization": token
-                    }
-            elif Ins == ("missionapi.appx.co.in"):
-                hdr11 = {
-                    "Host": "missionapi.appx.co.in",
-                    "Client-Service": "Appx",
-                    "Auth-Key": "appxapi",
-                    "User-Id": userid,
-                    "Authorization": token
-                    }
-            else: 
-                hdr11 = {
-                    "Host": "rozgarapinew.teachx.in",
-                    "Client-Service": "Appx",
-                    "Auth-Key": "appxapi",
-                    "User-Id": userid,
-                    "Authorization": token
-                    }            
-            if Ins == ("lastexamapi.teachx.in"):
-                res4 = requests.get("https://lastexamapi.teachx.in/get/livecourseclassbycoursesubtopconceptapiv3?topicid=" + t + "&start=-1&courseid=" + raw_text2 + "&subjectid=" + raw_text3,headers=hdr11).json()
-            elif Ins == ("missionapi.appx.co.in"):
-                res4 = requests.get("https://missionapi.appx.co.in/get/livecourseclassbycoursesubtopconceptapiv3?topicid=" + t + "&start=-1&conceptid=4&courseid=" + raw_text2 + "&subjectid=" + raw_text3,headers=hdr11).json()
+        mm = "Ankit-With-Rojgar"
+        t_name = b_data2[0]["topic_name"]
+        file_path = f"{mm}{t_name}.txt"
+
+        for tid in topic_ids:
+            if "lastexamapi" in Ins:
+                url = f"https://{Ins}/get/livecourseclassbycoursesubtopconceptapiv3?topicid={tid}&start=-1&courseid={course_id}&subjectid={subject_id}"
+            elif "missionapi" in Ins:
+                url = f"https://{Ins}/get/livecourseclassbycoursesubtopconceptapiv3?topicid={tid}&start=-1&conceptid=4&courseid={course_id}&subjectid={subject_id}"
             else:
-                res4 = requests.get("https://rozgarapinew.teachx.in/get/livecourseclassbycoursesubtopconceptapiv3?topicid=" + t + "&start=-1&conceptid=1&courseid=" + raw_text2 + "&subjectid=" + raw_text3,headers=hdr11).json()
+                url = f"https://{Ins}/get/livecourseclassbycoursesubtopconceptapiv3?topicid={tid}&start=-1&conceptid=1&courseid={course_id}&subjectid={subject_id}"
 
+            res4 = requests.get(url, headers=hdr1).json()
             topicid = res4["data"]
-            vj = ""
-            for data in topicid:
-                tids = (data["Title"])
-                idid = f"{tids}"
-                if len(f"{vj}{idid}") > 9999:
-                    vj = ""
-                vj += idid
 
-            vp = ""
             for data in topicid:
-                tn = (data["download_link"])
-                tns = f"{tn}"
-                if len(f"{vp}{tn}") > 9999:
-                    vp = ""
-        # print("Download Links: \n", tns
-                vp += tn
-            vs = ""
-            for data in topicid:
-                tn0 = (data["pdf_link"])
-                tns0 = f"{tn0}"
-                if len(f"{vs}{tn0}") > 9999:
-                    vs = ""
-            # print("Download Links: \n", tns
-                vs += tn0
-            cool2 = ""
-            #BBB1 = ''
-            for data in topicid:
-                if data["download_link"]:
-                    b64 = (data["download_link"])
-                else:
-                    b64 = (data["pdf_link"])
-                tid = data["Title"].replace(" : ", " ").replace(" :- ", " ").replace(" :-", " ").replace(":-", " ").replace("_", " ").replace("(", "").replace(")", "").replace("&", "").strip()
-                zz = len(tid)
+                b64 = data.get("download_link") or data.get("pdf_link")
+                tid_title = re.sub(r"[^\w\s]", "", data["Title"])
                 key = "638udh3829162018".encode("utf8")
                 iv = "fedcba9876543210".encode("utf8")
                 ciphertext = bytearray.fromhex(b64decode(b64.encode()).hex())
                 cipher = AES.new(key, AES.MODE_CBC, iv)
                 plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
-                #print(plaintext)
-                b=plaintext.decode('utf-8')
-                cc0 = (f"{tid}:{b}")
-                if len(f'{cool2}{cc0}') > 9999:
-                    ##await m.reply_text(hh)
-                    cool2 = ""
-                cool2 += cc0
-                mm = "Ankit-Wih-Rojgar"
-                #await m.reply_text(BBB1, hh)
-                
-                with open(f'{mm}{t_name}.txt', 'a') as f:
-                    f.write(f"{tid}:{b}\n")
-        await m.reply_document(f"{mm}{t_name}.txt")
+                url = plaintext.decode('utf-8')
+
+                with open(file_path, 'a') as f:
+                    f.write(f"{tid_title}:{url}\n")
+
+        await m.reply_document(file_path)
     except Exception as e:
-        await m.reply_text(str(e))
+        await m.reply_text(f"Error: {e}")
+
     await m.reply_text("Done")
